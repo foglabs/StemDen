387dc7ab4794d46439d6e0ddf88a2ea6503e332d
Henry Neels <foglaboratories@gmail.com>
2014-10-10 13:34:34 -0400

    Hey!

--- db/migrate/20141010171211_add_category_to_songs.rb
+++ db/migrate/20141010171211_add_category_to_songs.rb
@@ -1,9 +1,11 @@
 class AddCategoryToSongs < ActiveRecord::Migration
   def up
     add_column :songs, :category, :string
+    add_column :songs, :desc, :string
   end
 
   def down
     remove_column :songs, :category
+    remove_column :songs, :desc
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20141010171211) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "category"
+    t.string   "desc"
   end
 
   create_table "users", force: true do |t|
